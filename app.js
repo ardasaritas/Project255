cc8cc8870c7b7471b65df26d0d3876a462191bd9
Işık Dönger <isikdonger04@gmail.com>
2024-12-21 20:33:27 +0300

    Initialization

---
+++ app.js
@@ -0,0 +1,6 @@
+let states = {}
+
+states = {active: null, users: [{name: "Işık Dönger", wallet: {cash: 1000}}, 
+                                {name: "İbrahim Can Doğan", wallet: {cash: 1000}}, 
+                                {name: "Arda Sarıtaş", wallet: {cash: 1000}}, 
+                                {name: "Yağız Çetin", wallet: {cash: 1000}}]}
